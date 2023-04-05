c39f2d1e25cd8d3ef8b5d43558fcad9370d4cb41
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-04-06 00:08:13 +0600

    Till Fetching Products from Database

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,34 +4,35 @@ import Card from "../utils/Card";
 import Review from "./Review";
 import { Link } from "react-router-dom";
 
-function Product() {
-  const reviewed = true;
+function Product(props: any) {
+  const { title, category, image, review, reviewCount, price, isLoved } = props;
   return (
     <div className="card-hover">
       <Card>
         <div className="w-full rounded-lg overflow-hidden">
           <Link to="/product-details" className="cursor-pointer">
             <img
-              className="w-full hover-effect-product-img"
-              src="https://static.vecteezy.com/system/resources/thumbnails/009/384/620/small/fresh-pizza-and-pizza-box-clipart-design-illustration-free-png.png"
+              className="w-full hover-effect-product-img h-52"
+              src={
+                image ||
+                "https://static.vecteezy.com/system/resources/thumbnails/009/384/620/small/fresh-pizza-and-pizza-box-clipart-design-illustration-free-png.png"
+              }
               alt=""
             />
           </Link>
         </div>
         <div className="flex justify-between items-center">
-          <h4 className="text-sm my-2 font-semibold leading-0">Category</h4>
-          <Review reviewCount={2.8} />
+          <h4 className="text-sm my-2 font-semibold leading-0">{category}</h4>
+          <Review reviewCount={reviewCount} review={review} />
         </div>
         <div className="flex justify-between">
           <div>
             <Link className="cursor-pointer" to="/product-details">
-              <h2 className="text-lg font-semibold m-0 leading-5">
-                This is title helle ev how are you
-              </h2>
+              <h2 className="text-lg font-semibold m-0 leading-5">{title}</h2>
             </Link>
           </div>
           {/* <p className="text-xl font-bold leading-5 text-red-700"> */}
-          {reviewed ? (
+          {isLoved ? (
             <HeartFilled className="text-xl leading-5 cursor-pointer text-orange-600" />
           ) : (
             <HeartOutlined className="text-xl leading-5 cursor-pointer" />
@@ -40,7 +41,7 @@ function Product() {
         </div>
         <div className="w-full flex justify-between items-center mt-2">
           <p className="text-md font-semibold leading-5 text-blue-600">
-            Price: <span className="text-red-700 font-normal">200tk</span>
+            Price: <span className="text-red-700 font-normal">{price} tk</span>
           </p>
           <button className="text-sm rounded-md font-bold p-2 px-3 flex justify-center items-center bg-black outline-none text-white max-w-24 hover:text-black hover:shadow-lg hover:bg-orange-500">
             Add to cart
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,31 +1,58 @@
 import { Divider, Select } from "antd";
 import Product from "./Product";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Products({ screen }: any) {
+  const [products, setProducts] = useState<any>([]);
   const handleChange = (value: { value: string; label: React.ReactNode }) => {
     console.log(value);
   };
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get("/products/get/products");
+      console.log("Data: ", data.data);
+      setProducts(data.data);
+    };
+    getData();
+  }, []);
   return (
     <div className="bg-slate-100 px-16 py-6">
       <Divider orientation="left">Products</Divider>
-      <Select
-        labelInValue
-        defaultValue={{ value: "lucy", label: "Lucy (101)" }}
-        style={{ width: 120 }}
-        onChange={handleChange}
-        options={[
-          {
-            value: "jack",
-            label: "Jack (100)",
-          },
-          {
-            value: "lucy",
-            label: "Lucy (101)",
-          },
-        ]}
-      />
+      {screen !== "home" ? (
+        <Select
+          labelInValue
+          defaultValue={{ value: "lucy", label: "Lucy (101)" }}
+          style={{ width: 120 }}
+          onChange={handleChange}
+          options={[
+            {
+              value: "jack",
+              label: "Jack (100)",
+            },
+            {
+              value: "lucy",
+              label: "Lucy (101)",
+            },
+          ]}
+        />
+      ) : null}
       <div className="products-grid mt-10">
+        {products.map((product: any) => (
+          <Product
+            key={product._id}
+            title={product.title}
+            category={product.category}
+            image={product.image}
+            review={product.review}
+            reviewCount={product.reviewCount}
+            price={product.price}
+            isLoved={product.isLoved}
+          />
+        ))}
+        {/* <Product />
         <Product />
         <Product />
         <Product />
@@ -35,9 +62,7 @@ function Products({ screen }: any) {
         <Product />
         <Product />
         <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product /> */}
       </div>
       {screen === "products" ? null : (
         <div className="text-center my-7">
